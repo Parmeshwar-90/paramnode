beaad7b49ad7cdc28ed52d41c2abaa36db33b095
perennial <parmeshwarpatil956@gmail.com>
2017-04-18 12:30:09 +0530

    task done

--- public/js/main.js
+++ public/js/main.js
@@ -1,4 +1,5 @@
 $(document).ready(function(){
+	var flag = "by-time-slot";
 	$('.get-quote-btn').click(function(){
 		$.ajax({
 			type: 'post',
@@ -6,8 +7,6 @@ $(document).ready(function(){
 			dataType: 'json',
 			success: function(res) {
 				response = JSON.parse(res)
-				//console.log('-------- res: ' + response.quote);
-				//console.log('-------- res: ' + response.author);
 				$('.quote').html(response.quote);
 				$('.author').html(response.author);
 			},
@@ -49,7 +48,6 @@ $(document).ready(function(){
 				 obj = JSON.parse(res);
 				obj.articles.forEach( function (arrayItem)
 				{
-				    //console.log(arrayItem.title);
 				    str += '<div class="container"><div class="row"><div class="col-sm-4"><div class="news"><div class="img-figure"><div class="cat">'+arrayItem.author+'</div><img src="'+arrayItem.urlToImage+'" class="img-responsive"></div><div class="title"><h3>'+arrayItem.title+'</h3></div><p class="description">'+arrayItem.description+'</p></div></div></div></div>';
 
 				});
@@ -65,39 +63,51 @@ $(document).ready(function(){
 	$('#by-time-slot').show();
 	$('#by-frequenc').hide();
 	//$('#no_of_days').val(1);
+	$('#by_time_slot').val(1);
 	$('.btntype').click(function(){
+
 		var v = $(this).attr('rel');
 		if(v == "by-time-slot"){
 			$('#by-time-slot').show();
 			$('#by-frequenc').hide();
+			$('#by_time_slot').val(1);
+			$('#by_time_frequncy').val(0);
+			flag = "by-time-slot";
 		}
 		if(v == "by-frequency"){
 			$('#by-frequenc').show();
 			$('#by-time-slot').hide();
+			$('#by_time_slot').val(0);
+			$('#by_time_frequncy').val(1);
+			flag = "by-frequenc";
 		}
 
 	});
 
 	$("#patient-schedule").submit(function(){
-	    if($(".cbk").prop('checked') != true){
-    		$("#myModal").modal({                    
-		      "backdrop"  : "static",
-		      "keyboard"  : true,
-		      "show"      : true                     
-		    });
-    		return false;
-		}
 
-		var days = $('#no_of_days').val();
-		if(days <= 0){
-			$("#daysModal").modal({                    
-		      "backdrop"  : "static",
-		      "keyboard"  : true,
-		      "show"      : true                     
-		    });
-    		return false;
-		}
+	    if(flag == "by-time-slot"){
+	    	if($(".cbk").prop('checked') != true){
+	    		$("#myModal").modal({                    
+			      "backdrop"  : "static",
+			      "keyboard"  : true,
+			      "show"      : true                     
+			    });
+	    		return false;
+			}
+	    }
 
+	    if(flag == "by-frequenc"){
+	    	var days = $('#no_of_days').val();
+			if(days <= 0){
+				$("#daysModal").modal({                    
+			      "backdrop"  : "static",
+			      "keyboard"  : true,
+			      "show"      : true                     
+			    });
+	    		return false;
+			}
+	    }
 	});
     
 
--- routes/sch.js
+++ routes/sch.js
@@ -9,18 +9,38 @@ router.get('/', function(req, res){
 });
 
 router.post('/show', function(req, res){
-	res.render('info', {
-		before_breakfast : req.body.before_breakfast,
-		before_lunch : req.body.before_lunch,
-		before_dinner : req.body.before_dinner,
-		before_suffer : req.body.before_suffer,
-		after_breakfast : req.body.after_breakfast,
-		after_lunch : req.body.after_lunch,
-		after_dinner : req.body.after_dinner,
-		after_suffer : req.body.after_suffer,
-		days : req.body.no_of_days,
-		day_iteration : req.body.day_iteration
-	});
+	var by_time_slot = req.body.by_time_slot;
+	var by_time_frequncy = req.body.by_time_frequncy;
+	if(by_time_slot > 0){
+		res.render('info', {
+			before_breakfast : req.body.before_breakfast,
+			before_lunch : req.body.before_lunch,
+			before_dinner : req.body.before_dinner,
+			before_suffer : req.body.before_suffer,
+			after_breakfast : req.body.after_breakfast,
+			after_lunch : req.body.after_lunch,
+			after_dinner : req.body.after_dinner,
+			after_suffer : req.body.after_suffer,
+			days : req.body.no_of_days,
+			day_iteration : req.body.day_iteration,
+			by_time_slot : req.body.by_time_slot,
+		});
+	}
+	if(by_time_frequncy > 0){
+		res.render('info', {
+			before_breakfast : req.body.before_breakfast,
+			before_lunch : req.body.before_lunch,
+			before_dinner : req.body.before_dinner,
+			before_suffer : req.body.before_suffer,
+			after_breakfast : req.body.after_breakfast,
+			after_lunch : req.body.after_lunch,
+			after_dinner : req.body.after_dinner,
+			after_suffer : req.body.after_suffer,
+			days : req.body.no_of_days,
+			day_iteration : req.body.day_iteration,
+			by_time_frequncy : req.body.by_time_frequncy
+		});
+	}
 });
 
 
